944a8dd7aec56cb1b988efd7d78ac347c3cfafca
dlfud <99ysl@naver.com>
2022-09-06 17:17:08 +0900

    layout, router

--- front/src/index.js
+++ front/src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about/:id" element={<About />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
